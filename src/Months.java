598b0d4bf5eaec517ab11462a8fb1822a1bf4fb8
vitko5 <cp.advert0@googlemail.com>
2022-07-07 21:07:28 +0300

    update

--- src/Months.java
+++ src/Months.java
@@ -1,21 +1,34 @@
+import java.time.LocalDate;
+
 public enum Months {
-    JANUARY((byte)31,Seasons.WINTER),
-    FEBRUARY((byte)28,Seasons.WINTER),
-    MARCH((byte)31,Seasons.SPRING),
-    APRIL((byte)30,Seasons.SPRING),
-    MAY((byte)31,Seasons.SPRING),
-    JUNE((byte)30,Seasons.SUMMER),
-    JULY((byte)31,Seasons.SUMMER),
-    AUGUST((byte)31,Seasons.SUMMER),
-    SEPTEMBER((byte)30,Seasons.AUTUMN),
-    OCTOBER((byte)31,Seasons.AUTUMN),
-    NOVEMBER((byte)30,Seasons.AUTUMN),
-    DECEMBER((byte)31,Seasons.WINTER);
+    JANUARY(31, Seasons.WINTER),
+    FEBRUARY(28,Seasons.WINTER),
+    MARCH(31,Seasons.SPRING),
+    APRIL(30,Seasons.SPRING),
+    MAY(31,Seasons.SPRING),
+    JUNE(30,Seasons.SUMMER),
+    JULY(31,Seasons.SUMMER),
+    AUGUST(31,Seasons.SUMMER),
+    SEPTEMBER(30,Seasons.AUTUMN),
+    OCTOBER(31,Seasons.AUTUMN),
+    NOVEMBER(30,Seasons.AUTUMN),
+    DECEMBER(31,Seasons.WINTER);
     private final byte days;
     private final Seasons season;
 
-    Months(byte d, Seasons s) {
-        this.days = d;
+    Months(int d, Seasons s) {
+        LocalDate localDate = LocalDate.now();
+        if (this.name().equals("FEBRUARY")) {
+            if (localDate.isLeapYear()) {
+                this.days = 29;
+            }
+            else {
+                this.days = 28;
+            }
+        }
+        else {
+            this.days = (byte) d;
+        }
         this.season = s;
     }
 
